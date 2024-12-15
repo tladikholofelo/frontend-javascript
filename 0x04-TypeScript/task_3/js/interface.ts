35be00da1c9953b7af9eedac24ccb8ed746c4f13
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2024-12-15 17:08:40 +0200

    Create task_3/js/interface.ts

---
+++ 0x04-TypeScript/task_3/js/interface.ts
@@ -0,0 +1,11 @@
+// task_3/js/interface.ts
+
+// Type definition for RowID
+export type RowID = number;
+
+// Interface definition for RowElement
+export interface RowElement {
+  firstName: string;
+  lastName: string;
+  age?: number;  // Optional field
+}
