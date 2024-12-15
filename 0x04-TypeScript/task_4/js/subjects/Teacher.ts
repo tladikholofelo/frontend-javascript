7f3f54c6cdb0a1eec52385dd0c4977f018d6fdc9
Kholofelo Tladi <tladi.kholofelo@gmail.com>
2024-12-15 18:08:47 +0200

    Create task_4/js/subjects/Teacher.ts

---
+++ 0x04-TypeScript/task_4/js/subjects/Teacher.ts
@@ -0,0 +1,11 @@
+// task_4/js/subjects/Teacher.ts
+
+// Declare the Subjects namespace
+namespace Subjects {
+    // Teacher interface
+    export interface Teacher {
+      firstName: string;
+      lastName: string;
+    }
+  }
+  
